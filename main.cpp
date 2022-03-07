b0f8671070d664de919b00e6ea85fe69846231e2
ye da goat fr <100773794+tankmankirils1989@users.noreply.github.com>
2022-03-07 10:57:29 +0200

    Update main.cpp
    xd

--- main.cpp
+++ main.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class twointegers
-{
-public:
+class twointegers {
+private:
     int a;
     int b;
+public:
+int sum=a+b;
 };
-
 int main()
 {
-    twointegers t;
-    t.a = 5;
-    t.b = 7;
-    cout << t.a+t.b << endl;
+    twointegers t(5,7);
+    cout << t.sum << endl;
     return 0;
 }
-
-/*
-int main()
-{
-    twointegers t (5, 7);
-    cout << t.sum() << endl;
-    return 0;
-}
-*/
