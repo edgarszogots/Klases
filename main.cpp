1b70e599cbe158b958a0e44bdb91bbfa0aff0a84
IevaOnzeva <100773760+IevaOnzeva@users.noreply.github.com>
2022-03-07 11:16:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,18 @@
+
 #include <iostream>
 using namespace std;
 
 class twointegers
 {
-public:
+private:
     int a;
     int b;
 };
 
 int main()
 {
-    twointegers t;
-    t.a = 5;
-    t.b = 7;
-    cout << t.a+t.b << endl;
-    return 0;
-}
-
-/*
-int main()
-{
     twointegers t (5, 7);
     cout << t.sum() << endl;
     return 0;
 }
-*/
+
